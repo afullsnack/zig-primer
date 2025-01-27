e21b8b875ea51b0712603aa81dfffbe2040c7f2f
Miracle Friday <miraclef60@gmail.com>
2025-01-27 14:37:35 +0100

    feat: Comptime to run code during compilation times

---
+++ src/comptime.zig
@@ -0,0 +1,16 @@
+// comptime to execute code at comilation timete
+const expect = @import("std").testing.expect;
+
+fn fibonacci(n: u16) u16 {
+    if (n == 0 or n == 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+test "comptime blocks" {
+    const x = comptime fibonacci(10);
+    const y = comptime blk: {
+        break :blk fibonacci(10);
+    };
+
+    try expect(x == 55);
+    try expect(y == 55);
+}
