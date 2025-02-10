3346cdcadc0da07385f2ae712e8ef078f74bb8ed
Miracle Friday <miraclef60@gmail.com>
2025-02-10 23:27:27 +0100

    refactor: Write a connector that connects to a host address and port

--- src/random_finds.zig
+++ src/random_finds.zig
@@ -3,16 +3,43 @@ const builtin = @import("builtin"); // explore the builtin package
 
 const print = std.debug.print;
 
-test "what os" {
-    const os = builtin.os.tag;
-    print("Current operating system is: {}\n", .{os});
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
 
-    const string_literal =
-        \\ this is a shitty string literal
-        \\ maybe we can use this for some up coming mods
-        \\ or look into the type
-    ;
+    const alloc = gpa.allocator();
+    const connection = try std.net.tcpConnectToHost(alloc, "localhost", 8080);
+    print("Connection info: {}", .{connection});
+    _ = connection.close();
 
-    print("Type of string literal {}\n", .{@TypeOf(string_literal)});
-    print("{s}\n", .{string_literal});
+    while (true) {
+        std.time.sleep(1_000_000_000);
+    }
 }
+
+test "understanding GPA" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const alloc = gpa.allocator();
+    const connection = try std.net.tcpConnectToHost(alloc, "localhost", 8000);
+    _ = connection;
+
+    while (true) {
+        std.time.sleep(1_000_000_000);
+    }
+}
+
+// test "what os" {
+//     const os = builtin.os.tag;
+//     print("Current operating system is: {}\n", .{os});
+
+//     const string_literal =
+//         \\ this is a shitty string literal
+//         \\ maybe we can use this for some up coming mods
+//         \\ or look into the type
+//     ;
+
+//     print("Type of string literal {}\n", .{@TypeOf(string_literal)});
+//     print("{s}\n", .{string_literal});
+// }
